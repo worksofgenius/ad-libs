4425b8fabc7ff75f04d9cdbc479b2a89d33e3b3f
Marisa <54582562+worksofgenius@users.noreply.github.com>
2021-06-08 22:07:52 +0900

    Update quote

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const secondVerb = prompt('Choose another verb.');
 const noun = prompt('Choose a plural noun.');
 const exclamation = prompt('Choose an exclamation.');
 
-const quote = `<p>The only people that interest me are the ${adjective} ones, the ones who are ${adjective} to live, ${adjective} to talk, ${adjective} to be saved, desirous of everything at the same time, the ones that never ${verb} or say a commonplace thing but ${secondVerb}, ${secondVerb}, ${secondVerb} like fabulous yellow roman candles exploding like ${noun} across the stars and in the middle you see the blue centerlight pop and everybody goes ‘${exclamation}!’</p>`;
+const quote = `<p>The only people that interest me are the ${adjective} ones, the ones who are ${adjective} to live, ${adjective} to talk, ${adjective} to be saved, desirous of everything at the same time, the ones that never ${verb} or say a commonplace thing but ${secondVerb}, ${secondVerb}, ${secondVerb} like fabulous yellow roman candles exploding like ${noun} across the stars and in the middle you see the blue centerlight pop and everybody goes '${exclamation}!'</p>`;
 console.log(quote);
 
 document.querySelector('main').innerHTML = quote;
